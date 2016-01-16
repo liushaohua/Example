d5cd21cd2ac53c8c074a65f7ea3dc09d0330a0dd
shaohua.liu <fe.liushaohua@gmail.com>
2016-01-16 18:38:15 +0800

    add /e_updata

--- git_hooks/index.js
+++ git_hooks/index.js
@@ -3,17 +3,19 @@ var Url = require('url');
 var Fs = require("fs");
 var Path = require("path");
 var Child_process = require('child_process');
-var root = '../blog/';
+var root = {
+	'/update': '../blog/',
+	'/e_update': '../Example'
+};
 
 
 http.createServer(function (req, res) {
 	var url = Url.parse(req.url).pathname;
 
-    if(url === '/update'){
-        Child_process.exec('cd '+ root +' && git pull', function(a, b){
+    if(root[url]){
+        Child_process.exec('cd '+ root[url] +' && git pull', function(a, b){
             console.log(a, b);
         });
-		console.log('pull');
 
         res.writeHead(200, {
             "Content-Type": "text/html"
